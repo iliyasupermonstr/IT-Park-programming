75f261b9b086d06882ae485ba0abfdeada4de2b6
iliyasupermonstr <94247348+iliyasupermonstr@users.noreply.github.com>
2023-12-29 17:16:17 +0300

    addToSelfWork

--- Self_work/Animals.py
+++ Self_work/Animals.py
@@ -11,14 +11,26 @@ class Animal:
 
     def go(self):
         print("Bye")
+class Person:
+    def __init__(self, brain = Brain()):
+        self.brain = brain
+    
+    def say(self):
+        self.brain.say()
+
+    def go(self):
+        print("Пока,хорошего дня!")
 
 class DogBrain():
     def say(self):
         print("Гав-Гав")
 
-class CatBrain():
+class CatBrain:
     def say(self):
         print("Mew")
+class CowBrain:
+    def say(self):
+        print("Муууу")
 
 class FrankensteinBrain:
     def __init__(self, brains):
@@ -26,6 +38,10 @@ class FrankensteinBrain:
     def say(self):
         for brain in self.brains:
             brain.say()
+
+class PersonBrain:
+    def say(self):
+        print('Привет,как дела?')
         
 class Cat(Animal):
     def __init__(self,brain = CatBrain()):
@@ -34,6 +50,15 @@ class Cat(Animal):
 class Dog(Animal):
     def __init__(self,brain = DogBrain()):
         super().__init__(brain)
+class Cow(Animal):
+    def __init__(self,brain = CowBrain()):
+        super().__init__(brain)
+
+class Russian(Person):
+    def __init__(self,brain = PersonBrain()):
+        super().__init__(brain)
+
+
 
 
 Leona = Cat()
@@ -45,7 +70,10 @@ Sherlok = Dog()
 Sherlok.say()
 Sherlok.go()
 
-Frankenstein = Dog(brain = FrankensteinBrain([DogBrain(),CatBrain()]))
-Frankenstein.say()
-Frankenstein.go()
+Murka = Cow()
+Murka.say()
+Murka.go()
 
+Ivan = Russian()
+Ivan.say()
+Ivan.go()
