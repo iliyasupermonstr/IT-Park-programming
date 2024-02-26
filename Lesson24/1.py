0822424c6b52331a993a7188a064579aaed151c7
iliyasupermonstr <94247348+iliyasupermonstr@users.noreply.github.com>
2024-02-26 20:51:24 +0300

    pon

--- Lesson24/1.py
+++ Lesson24/1.py
@@ -1,11 +1,46 @@
-from datetime import datetime,time
-from time import sleep
-import playsound
 from typing import Dict
 import os
 from argparse import ArgumentParser
-import json 
+from datetime import datetime, time
+from time import sleep
+import playsound
+import json
+
 SIGNAL = "/Users/iliyabezrukov/Downloads/12-milky-way.mp3"
+CLOCK_INTERVAL_SEC =0.1
+TIME_FORMAT = "%H:%M:%S"
+
+def save_alarm_info(name,time_value,repeats_count):
+    json_obj ={
+        "name":name,
+        "time":time_value.strftime(TIME_FORMAT),
+        "repeat": repeats_count
+    }
+    alarm_info = get_alarm_info()
+    for i in range(len(alarm_info)):
+        alarm_info[i]["time"] = alarm_info[i]["time"].strftime(TIME_FORMAT)
+    alarm_info.append(json_obj)
+
+    with open("data_file.json,'w") as file_ctx:
+        json.dump(alarm_info,file_ctx,indent=4)
+def get_alarm_info(file_path="data_file.json"):
+    if os.path.exists("data_file.json"):
+        with open("data_file.json") as file_ctx:
+            lines = file_ctx.read()
+            json_obj = json.loads(lines)
+    else:
+        json_obj = []
+    for i in range(len(json_obj)):
+        json_obj[i]["time"] = datetime.strptime(json_obj[i]["time"],TIME_FORMAT)
+    return json_obj
+def format_alarms(alarms_info):
+    if not alarms_info:
+        return "Сигналы будильника отсутствуют"
+    message_lines  = ["Сигналы будильника"]
+    for info in alarms_info:
+        line = f"{info['name']}:{info['time']} ({info['repeat']} раз)"
+        message_lines.append(line)
+    return "\n".join(message_lines)
 def is_wake_up(target_time: time) -> bool:
     while True:
         time_value = datetime.now().time()
@@ -28,14 +63,18 @@ parser = ArgumentParser(
 )
 
 parser.add_argument(
-    'time', type=str, help='Alarm time in format hh:mm:ss'
+    "--show",action ="store_true"
 )
 
-parser.add_argument("repeat",type = int,default = 5,help = "ALarm signal repeats count")
+# parser.add_argument("repeat",type = int,default = 5,help = "ALarm signal repeats count")
 arguments = parser.parse_args()
-time_str = arguments.time
-time_value = datetime.strptime(time_str,"%H:%M:%S")
+if arguments.show:
+    alarms_info = get_alarm_info()
+    formatting_text = format_alarms(alarms_info)
+    print(formatting_text)
+# time_str = arguments.time
+# time_value = datetime.strptime(time_str,"%H:%M:%S")
 
-repeats_count = arguments.repeat
+# repeats_count = arguments.repeat
 
-main(target_time = time_value,repeats_count = repeats_count)
+# main(target_time = time_value,repeats_count = repeats_count)
--- Lesson24/2.py
+++ Lesson24/2.py
@@ -1,3 +1,14 @@
 import json
+John =         {
+            "firstName": "John",
+            "age": 1
+        }
 with open("Example.json") as file_ctx:
-    print(file_ctx.read())
+    lines = file_ctx.read()
+    source_data = json.loads(lines)
+    print(source_data["firstName"])
+#source_data["hobbies"].append["drawing"]
+source_data["children"].append(John)
+with open("Example.json","w") as file_ctx:
+    json.dump(source_data,file_ctx,indent=4)
+
