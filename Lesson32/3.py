64479d538f1051a06a8b78bddc09b9afae647e16
iliyasupermonstr <94247348+iliyasupermonstr@users.noreply.github.com>
2024-04-14 19:52:07 +0300

    lesson32

--- Lesson32/3.py
+++ Lesson32/3.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtCore import pyqtSignal, QObject
-from PyQt5.QtGui import QMouseEvent
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication,QWidget,QInputDialog
+from PyQt5.QtGui import QColor,QFrame
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication,QWidget,QInputDialog,QLineEdit,QColorDialog
 
 class Communicate(QObject):
     close_app = pyqtSignal()
@@ -12,9 +12,15 @@ class Example(QWidget):
         self.init_ui()
 
     def init_ui(self):
+        colors = QColor(0,0,0)
         btn1 = QPushButton("Dialog", self)
         btn1.move(20, 20)
         btn1.clicked.connect(self.show_dialog)
+        self.frm = QFrame(self)
+        self.frm.setStyleSheet('QWidhet {background-color: %s}' % colors.name())
+        self.frm.setGeometry(130,22,100,100)
+        self.le = QLineEdit(self)
+        self.le.move(130,22)
         self.setGeometry(300, 300, 290, 150)
         self.setWindowTitle('Input Dialog')
         self.show()
